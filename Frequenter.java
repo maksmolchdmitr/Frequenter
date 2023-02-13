48c908041e7e99a405c7edcb1d3e70bad6aac97b
maksmolchdmitr <62752481+maksmolchdmitr@users.noreply.github.com>
2023-02-13 10:46:09 +0300

    add classes

---
+++ Frequenter.java
@@ -0,0 +1,52 @@
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Frequenter {
+
+    private FileReader fileReader;
+    private Map<Character, Long> frequency = new HashMap<>();
+    private TreeSet<Map.Entry<Character, Long>> sortedEntries = new TreeSet<>((e1, e2) -> {
+        int res = Long.compare(e1.getValue(), e2.getValue());
+        return (res==0)?(Character.compare(e1.getKey(), e2.getKey())):(res);
+    });
+    private long totalCount = 0;
+    private int maxSignCount = 1;
+    private long maxCount = 0;
+    public Frequenter(FileReader fileReader) {
+        this.fileReader = fileReader;
+        calcFrequency();
+    }
+    public void printStatistics(PrintStream out) {
+        for(Map.Entry<Character, Long> entry:sortedEntries){
+            out.printf("'%c':%"+maxSignCount+"d:%3d%%\n", entry.getKey(), entry.getValue(), entry.getValue()*100/totalCount);
+        }
+    }
+    private void calcFrequency(){
+        Scanner fileScanner = new Scanner(fileReader);
+        while (fileScanner.hasNext()){
+            for(Character s:fileScanner.next().toCharArray()){
+                if(((s >= 'a') && (s <= 'z')) || ((s >= 'A') && (s <= 'Z'))){
+                    totalCount++;
+                    if(frequency.containsKey(s)){
+                        frequency.replace(s, frequency.get(s)+1);
+                        maxCount = Math.max(frequency.get(s), maxCount);
+                    }else {
+                        frequency.put(s, 1L);
+                    }
+                }
+            }
+        }
+        int curSignCount = 0;
+        long curCount = maxCount;
+        while (curCount>0){
+            curSignCount++;
+            curCount/=10;
+        }
+        maxSignCount = curSignCount;
+        sortedEntries.addAll(frequency.entrySet());
+    }
+}
---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter file path whom statistic you want to see");
+        String filepath = scanner.next();
+        try {
+            Frequenter frequenter = new Frequenter(new FileReader(filepath));
+            frequenter.printStatistics(System.out);
+            System.out.println("Enter file path where you want to write statistic");
+            String outFilepath = scanner.next();
+            frequenter.printStatistics(new PrintStream(outFilepath));
+        } catch (FileNotFoundException e) {
+            System.out.println("File is not found!");
+        }
+    }
+}
